629f2c9927c527a8613e2b189a09a5b4689590e5
FeratKahraman <ahmetferat2003@gmail.com>
2022-05-04 19:54:26 -0400

    Test

--- TestModule/src/Package/classaaa.java
+++ TestModule/src/Package/classaaa.java
@@ -3,5 +3,6 @@ package Package;
 public class classaaa {
     public static void main(String[] args){
         System.out.println("Lalalalalalla");
+        System.out.println("Lalalalallalalalallalalallalalal");
     }
 }
